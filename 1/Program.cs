cea28af7f4aecc1900cac72500dabcbf7caa4cd8
DaryTalan <d-talanova@mail.ru>
2022-05-10 23:04:52 +0300

    git add

---
+++ 1/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] arr =
+{
+    "hello",
+    "2",
+    "world",
+    ":-)",
+    "1234",
+    "1567",
+    "-2",
+    "computer science",
+    "Russia",
+    "Denmark",
+    "Kazan",
+    "BY",
+};
+var result = new string[arr.Length];
+var realSize = 0;
+foreach (var value in arr)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
